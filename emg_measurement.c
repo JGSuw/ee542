9fb5ae029dd69d60a84f2714df67f7eb9f98c22a
Joseph Sullivan <jgs6156@uw.edu>
2017-05-30 14:09:14 -0700

    Update emg_measurement.c

--- emg_measurement.c
+++ emg_measurement.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include "emg_measurement.h"
 
-adc_meas_t measurement;
-adc_meas_queue_t output_queue;
-uint8_t slave_pin;
+#define ADC_SPI_TRANSACTION_SIZE 2
 
-int index = 0;
+static adc_meas_t measurement;
+static adc_meas_queue_t output_queue;
+
+static uint8_t slave_pin;
+
+static int index = 0;
+
+uint8_t adc_read_channel_1 = {byte1, byte2};
+
+uint8_t adc_read_channel_2 = {byte1, byte2}
 
 int queueInsert (adc_meas_t entry)
 {
-  if (index != output_queue.queue_size - 1)
+  if (index < output_queue.queue_size)
   {
-    output_queue.buffer[index] = entry;
-    index++;
-
+    output_queue.buffer[index++] = entry;
     return 0;
   }
 
@@ -28,13 +33,35 @@ bool queueFull (void)
   return false;
 }
 
+// TODO
+// Helper method called before ADC reads
+//
+void adcSPIconfig () {
+  // Configure slave pin
+  // Set slave pin polarity to ENABLE
+  // Set data mode of SPI1 interface
+  // Set bit order of SPI1 interface
+}
+
 adc_meas_t adcRead (void)
 {
-  measurement.channel_A_raw = transfer();
-  measurement.channel_A = 3.3 * channel_A_raw;
-  measurement.channel_B_raw = transfer();
-  measurement.channel_B = 3.3 * channel_A_raw;
-
+  // TODO
+  adcSPIconfig();
+  
+  // TODO - implement the SPI protocol used by the ADC
+  uint8_t measurement_buffer[2];
+  
+  transfer (adc_read_channel_1, measurement_buffer, ADC_SPI_TRANSACTION_SIZE, NULL);
+  
+  // TODO format raw data in measurement buffer
+  // Put integer representation and floating representation into measurement structure
+  
+  transfer (adc_read_channel_2, measurement_buffer, ADC_SPI_TRANSACTION_SIZE, NULL);
+  
+  // TODO format raw data in measurement buffer
+  // Put integer representaiton and floating representation into measurement structure
+  
+  // TODO set spi slave pin polarity to DISABLE
   return measurement;
 }
 
@@ -54,3 +81,5 @@ uint8_t queueReceive (adc_meas_t values[])
 
 return i;  
 }
+
+// TODO actual task implementation
