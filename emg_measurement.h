ccfa6a1af37229604b89b1a5b51fb8f507323021
Joseph Sullivan <jgs6156@uw.edu>
2017-05-30 13:51:41 -0700

    Update emg_measurement.h

--- emg_measurement.h
+++ emg_measurement.h
@@ -2,6 +2,8 @@
 #define EMG_HEADER
 #include <stdbool.h>
 
+#define BUFFER_SIZE 8
+
 typedef struct adc_meas_t{
   float channel_A;
   uint16_t channel_A_raw;
@@ -10,8 +12,8 @@ typedef struct adc_meas_t{
 };
 
 typedef struct adc_meas_queue_t{
-  const uint8_t queue_size;
-  adc_meas_t buffer[queue_size];
+  const uint8_t queue_size = BUFFER_SIZE;
+  adc_meas_t buffer[BUFFER_SIZE];
 };
 
 uint8_t queueReceive(adc_meas_t[]);
